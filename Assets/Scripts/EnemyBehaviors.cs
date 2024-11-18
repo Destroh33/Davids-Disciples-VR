08867b7b7797c9aadf034035695ae4e9a383b7a6
johanne <114450226+johannemoresco@users.noreply.github.com>
2024-11-17 17:17:55 -0800

    Initial ver of enemy pathfinding

--- Assets/Scripts/EnemyBehaviors.cs
+++ Assets/Scripts/EnemyBehaviors.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Pathfinding;
 
 public class EnemyBehaviors : MonoBehaviour
 {
@@ -9,33 +10,85 @@ public class EnemyBehaviors : MonoBehaviour
     private Transform tr;
     private Transform t;
     private bool playerContact = false;
-    [SerializeField] float speed;
     private bool canAttack = true;
-    private GameObject player;
+    [SerializeField] GameObject player;
+    private float chaseRange = 5f;
+    private bool isChasing = false;
+    [SerializeField] Transform[] walkPoints;
+
+    private int currentIndex = 0; 
+
+    private AILerp ai;
+    
+    void Awake() 
+    {
+        ai = GetComponent<AILerp>(); 
+    }
+
+    void Update() 
+    {
+        // Debug.Log(currentIndex);
+        if (isChasing) { Debug.Log("Chasing player"); }
+        else { Debug.Log("Patrolling"); }   
+        if (playerContact && canAttack)
+        {
+            Attack();
+        }
+        if(player != null && Vector3.Distance(transform.position, player.transform.position) <= chaseRange)
+        {
+            if(!isChasing)
+            {
+                isChasing = true;
+                ai.destination = player.transform.position;
+
+            }
+        }
+        if(isChasing && Vector3.Distance(transform.position, player.transform.position) <= chaseRange)
+        {
+            ai.destination = player.transform.position;
+        }
+        if(isChasing && Vector3.Distance(transform.position, player.transform.position) > chaseRange)
+        {
+            Debug.Log("in isChasing");
+            isChasing = false; 
+            ai.destination = walkPoints[currentIndex].transform.position;
+        }
+        else
+        {
+            if(!isChasing)
+            {
+                Patrol();
+            }
+        }
+    }
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         tr = GetComponent<Transform>();
         t = FindObjectOfType<PlayerMovement>().transform;
-    }
-    void setDirection()
-    {
-        tr.LookAt(t.position, tr.up);
-        tr.rotation = new Quaternion(tr.rotation.x, 0f, tr.rotation.z, 0f);
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        setDirection();
-        //Debug.Log(t.position);
-        rb.velocity = tr.forward*speed;
-        if (playerContact && canAttack)
-        {
-            Attack();
-        }
+    
+        ai.destination = walkPoints[currentIndex].position;
 
     }
+    // void setDirection()
+    // {
+    //     tr.LookAt(t.position, tr.up);
+    //     tr.rotation = new Quaternion(tr.rotation.x, 0f, tr.rotation.z, 0f);
+    // }
+
+    // // Update is called once per frame
+    // void Update()
+    // {
+    //     setDirection();
+    //     //Debug.Log(t.position);
+    //     rb.velocity = tr.forward*speed;
+    //     if (playerContact && canAttack)
+    //     {
+    //         Attack();
+    //     }
+
+    // }
     void Attack()
     {
         player.GetComponent<EntityHealthAndDmg>().TakeDamage(100);
@@ -60,5 +113,25 @@ public class EnemyBehaviors : MonoBehaviour
         {
             playerContact = false;
         }
+    }
+
+    void Patrol(){
+        //  Debug.Log("in patrol");
+        float distanceToWaypoint = Vector3.Distance(transform.position, walkPoints[currentIndex].position);
+        // Debug.Log("Enemy Position: " + transform.position + " | Checkpoint: " + walkPoints[currentIndex].position + " | Distance: " + distanceToWaypoint);
+        if (distanceToWaypoint < 6f)  
+        {
+            Debug.Log("in the if sttement");
+            currentIndex++;
+            if (currentIndex >= walkPoints.Length)
+            {
+                Debug.Log("in the if sttement 2s");
+                currentIndex = 0; 
+            }
+
+            ai.destination = walkPoints[currentIndex].position;
+        }
+
+
     }
 }
